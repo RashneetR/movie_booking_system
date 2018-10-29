e5b06c7f880046fbd1357580f274e117a03636b9
rashneet <rashneet@amuratech.com>
2018-10-29 19:18:47 +0530

    Edit aggregate query efficiency

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,19 +3,10 @@ class Admin::DashboardsController < ApplicationController
   before_action :set_admin_dashboard, only: %i[show edit update destroy]
 
   def index
-    @theatres = []
-    @movies = []
-    #@admin_movies = Show.collection.aggregate([{ '$group' => { '_id' => '$movie_id', 'total_revenue' => { '$sum' => { '$multiply' => ['$cost_per_seat', '$num_seats_sold'] } } } }])
-
-    @admin_movies = Show.collection.aggregate([{ '$group' => { '_id' => '$movie_id', 'total_revenue' => { '$sum' => { '$multiply' => ['$cost_per_seat', '$num_seats_sold'] } } , 'name' => '$movie.name' } }])
-    @admin_movies.each do |admin_movie|
-      @movie = Movie.find_by(id: admin_movie['_id'])
-      @movies << @movie.name
-    end
-    @admin_theatres = Show.collection.aggregate([{ '$group' => { '_id' => '$theatre_id', 'total_revenue' => { '$sum' => { '$multiply' => ['$cost_per_seat', '$num_seats_sold'] } } } }])
-    @admin_theatres.each do |admin_theatre|
-      @theatre = Theatre.find_by(id: admin_theatre['_id'])
-      @theatres << @theatre.name
-    end
+    @admin_movies = Show.collection.aggregate([{ '$group' => { '_id' => '$movie_id', 'total_revenue' => { '$sum' => { '$multiply' => ['$cost_per_seat', '$num_seats_sold'] } } } }])
+    @movies = Movie.in(:_id => @admin_movies.pluck(:_id)).pluck(:name)
+    @admin_theatres = Show.collection.aggregate([{ '$group' => { '_id' => '$theatre_id', 'total_revenue' => { '$sum' => { '$multiply' => ['$cost_per_seat', '$num_seats_sold'] } } } 
+      }])
+    @theatres = Theatre.in(:_id => @admin_theatres.pluck(:_id)).pluck(:name)
   end
 end
