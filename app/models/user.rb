cf33e63806754f35edf75ad44201c353a4fc238c
rashneet <rashneet@amuratech.com>
2018-11-21 18:31:49 +0530

    Edit delete old users rake task

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User
   include Mongoid::Timestamps
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable
+  #Scopes       
+  scope :inactive, -> { where(active: "inactive") }
 
   #Fields
   ## Database authenticatable
@@ -31,6 +33,7 @@ class User
   field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   field :locked_at,       type: Time
+
 
   #Indexes
   index({ email: 1 }, unique: true)
--- lib/tasks/delete_old_users.rake
+++ lib/tasks/delete_old_users.rake
@@ -1,15 +1,11 @@
 require 'rake'
 
 task delete_old_users: :environment do
-  old_users = User.where(active: 'inactive')
+  old_users = User.inactive.where(updated_at: 3.months.ago)
 
   old_users.each do |user|
-    next unless user.updated_at < 3.months.ago
-
-    t = Ticket.where(user_id: user.id)
-    r = Review.where(user_id: user.id)
-    t.each(&:destroy)
-    r.each(&:destroy)
+    Ticket.where(user_id: user.id).destroy_all
+    Review.where(user_id: user.id).destroy_all
     user.destroy
   end
 end
